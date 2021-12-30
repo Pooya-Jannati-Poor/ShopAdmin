0a2deeea136a2b1d1f3aad9e11e645459a84e1a2
Dragon <poyaj181@gmail.com>
2021-12-30 12:16:42 +0330

    add project

---
+++ app/src/main/java/ir/arinateam/shopadmin/AdminActivity.kt
@@ -0,0 +1,11 @@
+package ir.arinateam.shopadmin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.admin_activity)
+    }
+}
