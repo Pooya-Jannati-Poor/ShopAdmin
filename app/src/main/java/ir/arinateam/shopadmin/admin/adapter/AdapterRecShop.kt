b582c258bcf0ec885c2b9f4eb1d0833010a13eb9
Dragon <poyaj181@gmail.com>
2022-01-03 00:47:29 +0330

    ساخت 80% صفحه فروشگاه ها

--- app/src/main/java/ir/arinateam/shopadmin/admin/adapter/AdapterRecShop.kt
+++ app/src/main/java/ir/arinateam/shopadmin/admin/adapter/AdapterRecShop.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.appcompat.widget.SwitchCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.github.angads25.toggle.widget.LabeledSwitch
 import com.google.android.material.switchmaterial.SwitchMaterial
 import ir.arinateam.shopadmin.R
 import ir.arinateam.shopadmin.admin.interfaces.ChangeShopState
@@ -37,42 +38,22 @@ class AdapterRecShop(
 
         holder.tvShopName.text = model.shopName
 
-        holder.sbShopAvailable.isChecked = model.isActivated
-        holder.sbShopAvailable.jumpDrawablesToCurrentState()
+        holder.sbShopAvailable.isOn = model.isActivated
 
-        if (holder.sbShopAvailable.isChecked) {
+        holder.sbShopAvailable.setOnToggledListener { _, isOn ->
 
-            holder.tvEnabled.visibility = View.VISIBLE
-            holder.tvDisabled.visibility = View.INVISIBLE
-
-        } else {
-
-            holder.tvEnabled.visibility = View.INVISIBLE
-            holder.tvDisabled.visibility = View.VISIBLE
-
-        }
-
-
-        holder.sbShopAvailable.setOnCheckedChangeListener { _, isChecked ->
-
-            if (isChecked) {
-
-                holder.tvEnabled.visibility = View.VISIBLE
-                holder.tvDisabled.visibility = View.INVISIBLE
+            if (isOn) {
 
                 changeShopState.enabled()
 
             } else {
 
-                holder.tvEnabled.visibility = View.INVISIBLE
-                holder.tvDisabled.visibility = View.VISIBLE
-
                 changeShopState.disabled()
 
             }
 
-
         }
+
 
     }
 
@@ -81,9 +62,7 @@ class AdapterRecShop(
     inner class ItemAdapter(binding: LayoutRecShopBinding) : RecyclerView.ViewHolder(binding.root) {
 
         val tvShopName: TextView = binding.tvShopName
-        val sbShopAvailable: SwitchMaterial = binding.sbShopAvailable
-        val tvEnabled: TextView = binding.tvEnabled
-        val tvDisabled: TextView = binding.tvDisabled
+        val sbShopAvailable: LabeledSwitch = binding.sbShopAvailable
 
     }
 
