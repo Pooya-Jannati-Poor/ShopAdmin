44b5ee958fdedec1d200fd7bee745504fca0f252
Dragon <poyaj181@gmail.com>
2022-01-04 23:53:15 +0330

    تکمیل 10% صفحه پروفایل

---
+++ app/src/main/java/ir/arinateam/shopadmin/shop/ShopActivity.kt
@@ -0,0 +1,36 @@
+package ir.arinateam.shopadmin.shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ir.arinateam.shopadmin.R
+import ir.arinateam.shopadmin.databinding.ActivityShopBinding
+
+class ShopActivity : AppCompatActivity() {
+
+    private lateinit var bindingActivity: ActivityShopBinding
+
+    private lateinit var bottomNav: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingActivity = DataBindingUtil.setContentView(this, R.layout.activity_shop)
+
+        initView()
+
+        val navController = findNavController(R.id.fragment_controller_main)
+
+        bottomNav.setupWithNavController(navController)
+
+    }
+
+    private fun initView(){
+
+        bottomNav = bindingActivity.bottomNav
+
+    }
+
+}
